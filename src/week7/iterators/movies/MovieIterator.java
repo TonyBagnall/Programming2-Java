0428d5ade2be547ecc7fcd8a376816577112d72d
Tony Bagnall <ajb@uea.ac.uk>
2022-11-10 07:08:27 +0000

    add movies

---
+++ src/week7/iterators/movies/MovieIterator.java
@@ -0,0 +1,30 @@
+package week7.iterators.movies;
+
+import week7.iterators.movies.Movie;
+
+import java.util.Iterator;
+
+public class MovieIterator implements Iterator<Movie> {
+    MovieDatabase m;
+    int pos;
+    MovieIterator(MovieDatabase m){
+        this.m=m;
+        pos=m.size()-1;
+    }
+
+
+
+    @Override
+    public boolean hasNext() {
+        if(pos>=0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public Movie next() {
+        Movie movie=m.getMovie(pos);
+        pos--;
+        return movie;
+    }
+}
